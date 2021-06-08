233b510d761218045c0338853ba0aebd71ffea21
Rafael Breno <rafael_breno@outlook.com>
2021-06-08 19:52:16 -0300

    feat: Add error message constructor

--- errhandler.go
+++ errhandler.go
@@ -1,9 +1,25 @@
 package facio
 
+import "fmt"
+
 type ErrHandler struct {
 	// To check is there's an error or not
 	// Unexported to avoid user errors
 	isNil bool
+
+	// Error message
+	Error string
+}
+
+const (
+	msgInvalidValue = `The header %s received an invalid value: %s`
+)
+
+func NewError(msgTemplate string, values ...interface{}) ErrHandler {
+	return ErrHandler{
+		isNil: false,
+		Error: fmt.Sprintf(msgTemplate, values...),
+	}
 }
 
 // Create a new nil error
