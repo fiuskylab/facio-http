758dcca321a0aa9bfd98a3c6ae033f4e5f69523c
Rafael Breno <rafael_breno@outlook.com>
2021-06-08 19:53:32 -0300

    feat: Implement AddHeader method and Header struct field

--- facio.go
+++ facio.go
@@ -12,7 +12,7 @@ type Client struct {
 type Request struct {
 	// Headers sent to request
 	// ["Header Name": ["Header Value 1", "Header Value 2"]]
-	headers map[string]string
+	Headers map[string]string
 }
 
 // Response struct
@@ -22,13 +22,19 @@ type Response struct{}
 func NewClient(base string) Client {
 	return Client{
 		BaseURL: base,
-		Request: Request{},
+		Request: Request{
+			Headers: make(map[string]string),
+		},
 	}
 }
 
-func (r Request) AddHeader(header string, values ...string) Request {
-	value := strings.Join(values, ", ")
-	r.headers[header] = value
+func (c Client) AddHeader(header string, values ...string) (Request, ErrHandler) {
+	if len(values) == 0 {
+		return c.Request, NewError(msgInvalidValue, header, values)
+	}
 
-	return r
+	value := strings.Join(values, ", ")
+	c.Request.Headers[header] = value
+
+	return c.Request, NewNilError()
 }
