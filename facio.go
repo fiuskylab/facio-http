fa1d25d6968ed1c989725e4fd88c4126d0e4996b
Rafael Breno <rafael_breno@outlook.com>
2021-06-08 21:51:53 -0300

    feat: Add method to implement multiples headers using HeaderMap

--- facio.go
+++ facio.go
@@ -28,6 +28,7 @@ func NewClient(base string) Client {
 	}
 }
 
+// Add a single header with multiple values
 func (c Client) AddHeader(header string, values ...string) (Request, ErrHandler) {
 	if len(values) == 0 {
 		return c.Request, NewError(msgInvalidHeaderValue, header, values)
@@ -35,6 +36,26 @@ func (c Client) AddHeader(header string, values ...string) (Request, ErrHandler)
 
 	value := strings.Join(values, ", ")
 	c.Request.Headers[header] = value
+
+	return c.Request, NewNilError()
+}
+
+// Type to store a key with an array of strings
+// HeaderMap{
+// 		"Authorization": { "Bearer EEEYY.TOKEN.AZZ" },
+// 		"Accept-Encoding": { "gzip", "deflate" },
+// }
+type HeaderMap map[string][]string
+
+// Add multiple headers following the structure o HeaderMap
+func (c Client) AddHeaders(hr HeaderMap) (Request, ErrHandler) {
+	for headerName, headerValues := range hr {
+		if len(headerValues) == 0 {
+			return c.Request, NewError(msgInvalidHeaderValue, headerName, headerValues)
+		}
+		value := strings.Join(headerValues, ", ")
+		c.Request.Headers[headerName] = value
+	}
 
 	return c.Request, NewNilError()
 }
