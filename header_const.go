e588a874ca697e773b0fc60c418d29e627109611
Rafael Breno <rafael_breno@outlook.com>
2021-07-02 20:34:05 -0300

    feat: Add all Headers into getHeader method

--- header_const.go
+++ header_const.go
@@ -48,15 +48,90 @@ const (
 	XCSRFToken     // X-CSRF-Token Header
 )
 
+// Return the header name as string
 func (h header) getHeader() string {
 	switch h {
-	case Authorization:
-		return "Authorization"
+	// Standard Headers
+	case AIM:
+		return "A-IM"
+	case Accept:
+		return "Accept"
+	case AcceptCharset:
+		return "Accept-Charset"
 	case AcceptEncoding:
 		return "Accept-Encoding"
+	case AcceptLanguage:
+		return "Accept-Language"
+	case AcceptDatetime:
+		return "Accept-Datetime"
+	case AccessControlRequestMethod:
+		return "Access-Control-Request-Method"
+	case AccessControlRequestHeaders:
+		return "Access-Control-Request-Headers"
+	case Authorization:
+		return "Authorization"
+	case CacheControl:
+		return "Cache-Control"
+	case Connection:
+		return "Connection"
+	case ContentLength:
+		return "Content-Length"
 	case ContentType:
 		return "Content-Type"
+	case Cookie:
+		return "Cookie"
+	case Date:
+		return "Date"
+	case Expect:
+		return "Expect"
+	case Forwarded:
+		return "Forwarded"
+	case From:
+		return "From"
+	case Host:
+		return "Host"
+	case IfMatch:
+		return "If-Match"
+	case IfModifiedSince:
+		return "If-Modified-Since"
+	case IfNoneMatch:
+		return "If-None-Match"
+	case IfRange:
+		return "If-Range"
+	case IfUnmodifiedSince:
+		return "If-Unmodified-Since"
+	case MaxForwards:
+		return "Max-Forwards"
+	case Origin:
+		return "Origin"
+	case Pragma:
+		return "Pragma"
+	case ProxyAuthorization:
+		return "Proxy-Authorization"
+	case Range:
+		return "Range"
+	case Referer:
+		return "Referer"
+	case TE:
+		return "TE"
+	case UserAgent:
+		return "User-Agent"
+	case Upgrade:
+		return "Upgrade"
+	case Via:
+		return "Via"
+	case Warning:
+		return "Warning"
+
+	// Non-standard Headers
+	case Dnt:
+		return "Dnt"
+	case XRequestedWith:
+		return "X-Requested-With"
+	case XCSRFToken:
+		return "X-CSRF-Token"
 	default:
 		return ""
 	}
 }
+
