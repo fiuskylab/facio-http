c115344b6396d3e9ce64ecfdb808776e30626e0f
Rafael Breno <rafael_breno@outlook.com>
2021-07-01 21:07:41 -0300

    feat: Implement add header functions

--- client.go
+++ client.go
@@ -1,5 +1,7 @@
 package facio
 
+import "strings"
+
 // Client responsible to manage Requests and Responses
 type Client struct {
 	// URL that all requests will be made in
@@ -12,7 +14,32 @@ func NewClient(base string) Client {
 	return Client{
 		BaseURL: base,
 		Request: Request{
-			Headers: make(map[string]string),
+			Headers: make(HeaderResult),
 		},
 	}
+}
+
+// Add a single header with multiple values
+func (c Client) AddHeader(header string, values ...string) (Request, ErrHandler) {
+	if len(values) == 0 {
+		return c.Request, NewError(msgInvalidHeaderValue, header, values)
+	}
+
+	value := strings.Join(values, ", ")
+	c.Request.Headers[header] = value
+
+	return c.Request, NewNilError()
+}
+
+// Add multiple headers following the structure o HeaderMap
+func (c Client) AddHeaders(hr HeaderMap) (Request, ErrHandler) {
+	var err ErrHandler
+
+	c.Request.Headers, err = hr.build()
+
+	if !err.IsNil() {
+		return c.Request, err
+	}
+
+	return c.Request, NewNilError()
 }
