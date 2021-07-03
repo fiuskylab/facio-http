842600e7b0e2445d94125c8323e7e04ec71d91e0
Rafael Breno <rafael_breno@outlook.com>
2021-07-02 23:21:06 -0300

    feat: Add new request shortcuts

--- client.go
+++ client.go
@@ -63,3 +63,48 @@ func (c Client) NewRequest(method, endpoint string) (Request, ErrHandler) {
 
 	return c.Request, NewNilError()
 }
+
+// Shortcut for NewRequest("POST", "/foo")
+func (c Client) Post(endpoint string) (Request, ErrHandler) {
+	return c.NewRequest("POST", endpoint)
+}
+
+// Shortcut for NewRequest("GET", "/foo")
+func (c Client) Get(endpoint string) (Request, ErrHandler) {
+	return c.NewRequest("GET", endpoint)
+}
+
+// Shortcut for NewRequest("PUT", "/foo")
+func (c Client) Put(endpoint string) (Request, ErrHandler) {
+	return c.NewRequest("PUT", endpoint)
+}
+
+// Shortcut for NewRequest("PATCH", "/foo")
+func (c Client) Patch(endpoint string) (Request, ErrHandler) {
+	return c.NewRequest("PATCH", endpoint)
+}
+
+// Shortcut for NewRequest("DELETE", "/foo")
+func (c Client) Delete(endpoint string) (Request, ErrHandler) {
+	return c.NewRequest("DELETE", endpoint)
+}
+
+// Shortcut for NewRequest("HEAD", "/foo")
+func (c Client) Head(endpoint string) (Request, ErrHandler) {
+	return c.NewRequest("HEAD", endpoint)
+}
+
+// Shortcut for NewRequest("CONNECT", "/foo")
+func (c Client) Connect(endpoint string) (Request, ErrHandler) {
+	return c.NewRequest("CONNECT", endpoint)
+}
+
+// Shortcut for NewRequest("OPTIONS", "/foo")
+func (c Client) Options(endpoint string) (Request, ErrHandler) {
+	return c.NewRequest("OPTIONS", endpoint)
+}
+
+// Shortcut for NewRequest("TRACE", "/foo")
+func (c Client) Trace(endpoint string) (Request, ErrHandler) {
+	return c.NewRequest("TRACE", endpoint)
+}
