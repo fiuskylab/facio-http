420ff4df5e1c6df62d159033684ebd3c3f4a33fa
Rafael Breno <rafael_breno@outlook.com>
2021-07-04 19:40:29 -0300

    feat: Add URL builder into Request

--- facio.go
+++ facio.go
@@ -1,5 +1,7 @@
 package facio
 
+import "fmt"
+
 // Facio is the main struct to execute all actions and config the client
 type Facio struct {
 	// Client stores the main config
@@ -27,21 +29,28 @@ func (f *Facio) SetBaseURL(baseURL string) (*Facio, ErrHandler) {
 }
 
 // Request call into certain endpoint based on baseURL defined on client
-func (f *Facio) Request(method, endpoint string, hm HeaderMap) (*response, ErrHandler) {
+func (f *Facio) Request(method, endpoint string, hm HeaderMap) (*request, ErrHandler) {
 	var err ErrHandler
 
 	method, err = checkMethod(method)
 
 	if !err.isNil {
-		return &response{}, err
+		return &request{}, err
 	}
 
 	req := &request{
 		client:    f.client,
 		method:    method,
-		endpoint:  endpoint,
+		endpoint:  parseEndpoint(endpoint),
 		headerMap: hm,
 	}
 
+	req.url = fmt.Sprintf("%s%s", req.client.baseURL, req.endpoint)
+
+	return req, NewNilError()
+}
+
+// Do the given Request
+func (f *Facio) Do(req *request) (*response, ErrHandler) {
 	return req.call()
 }
