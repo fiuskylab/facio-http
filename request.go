5aaf7718be958550361dbca5e0be04b0c791b741
Rafael <rafael_breno@outlook.com>
2021-09-15 22:14:27 -0300

    feat: Add Headers related functions

--- request.go
+++ request.go
@@ -6,6 +6,8 @@ type Req struct {
 	Headers map[string]string
 }
 
+// NewRequest receives method and url to create
+// a base request to be made
 func NewRequest(method, url string) (*Req, error) {
 	var err error
 	var r Req
@@ -23,6 +25,19 @@ func NewRequest(method, url string) (*Req, error) {
 
 	r.BaseURL = url
 	r.Method = method
+	r.Headers = make(map[string]string)
 
 	return &r, err
+}
+
+// AddHeader add a header into request
+func (r *Req) AddHeader(name header, value string) *Req {
+	r.Headers[name.getHeader()] = value
+	return r
+}
+
+// AddCustomHeader add a custom header into request
+func (r *Req) AddCustomHeader(name, value string) *Req {
+	r.Headers[name] = value
+	return r
 }
