ee55ff252c9d8768380c722ec476127bf919dca7
Rafael <rafael_breno@outlook.com>
2021-09-15 23:25:46 -0300

    feat: Switch fields to unexported

--- request.go
+++ request.go
@@ -1,9 +1,10 @@
 package facio
 
 type Req struct {
-	BaseURL string
-	Method  string
-	Headers map[string]string
+	baseURL string
+	method  string
+	headers map[string]string
+	formURL map[string][]string
 }
 
 // NewRequest receives method and url to create
@@ -23,21 +24,31 @@ func NewRequest(method, url string) (*Req, error) {
 		return &r, err
 	}
 
-	r.BaseURL = url
-	r.Method = method
-	r.Headers = make(map[string]string)
+	r = Req{
+		baseURL: url,
+		method:  method,
+		headers: make(map[string]string),
+		formURL: make(map[string][]string),
+	}
 
 	return &r, err
 }
 
 // AddHeader add a header into request
 func (r *Req) AddHeader(name header, value string) *Req {
-	r.Headers[name.getHeader()] = value
+	r.headers[name.getHeader()] = value
 	return r
 }
 
 // AddCustomHeader add a custom header into request
 func (r *Req) AddCustomHeader(name, value string) *Req {
-	r.Headers[name] = value
+	r.headers[name] = value
+	return r
+}
+
+// SetURLForm receives a map with form-value variable
+func (r *Req) SetURLForm(vals map[string][]string) *Req {
+	r.formURL = vals
+
 	return r
 }
